3ca2b1785c22dda196f9175cdba19d51f340c925
Frostgold <nacho_andres@live.com>
2022-07-27 22:35:05 -0400

    Arreglo vistas para subida de archivos cronograma

--- cursos/models.py
+++ cursos/models.py
@@ -81,7 +81,6 @@ class CronogramaActividad(models.Model):
         super().save(*args, **kwargs)
 
         if self.archivo and self.is_image == True:
-            print(self.is_image)
             img = Image.open(self.archivo.path)
             if img.height > 720 or img.width > 1280:
                 output_size = (1280, 720)
